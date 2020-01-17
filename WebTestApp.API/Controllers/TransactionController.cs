e821b3782164fc7d6fcf2cfbf81ab61caac83e9f
Harbaruk <grishagarbaruk1995@gmail.com>
2020-01-17 12:55:57 +0200

    Basic infrastructure for the project

---
+++ WebTestApp.API/Controllers/TransactionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebTestApp.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/transaction")]
+    public class TransactionController : Controller
+    {
+        [HttpPost("upload")]
+        [ProducesResponseType(typeof(void), 200)]
+        [ProducesResponseType(typeof(string), 404)]
+        public IActionResult UploadFile([FromForm] IFormFile file)
+        {
+            return Ok();
+        }
+
+    }
+}
