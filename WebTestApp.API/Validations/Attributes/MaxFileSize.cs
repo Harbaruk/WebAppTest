4b002ef18c5db017fb7ef08ec6cde2874cadf87b
Harbaruk <grishagarbaruk1995@gmail.com>
2020-01-19 22:16:52 +0200

    filter attributes for file validation

---
+++ WebTestApp.API/Validations/Attributes/MaxFileSize.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace WebTestApp.API.Validations.Attributes
+{
+    public class MaxFileSizeAttribute : ValidationAttribute
+    {
+        private readonly int _maxFileSize;
+        public MaxFileSizeAttribute(int maxFileSize)
+        {
+            _maxFileSize = maxFileSize;
+        }
+
+        protected override ValidationResult IsValid(
+        object value, ValidationContext validationContext)
+        {
+            if (value is IFormFile file)
+            {
+                if (file.Length > _maxFileSize)
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+        public string GetErrorMessage()
+        {
+            return $"Maximum allowed file size is { _maxFileSize} bytes.";
+        }
+    }
+}
---
+++ WebTestApp.API/Validations/Attributes/SupportedFormat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace WebTestApp.API.Validations.Attributes
+{
+    public class SupportedFormatsAttribute : ValidationAttribute
+    {
+        private readonly string[] _supportedFormats;
+
+        public SupportedFormatsAttribute(params string[] supportedFormats)
+        {
+            _supportedFormats = supportedFormats;
+        }
+
+        protected override ValidationResult IsValid(
+        object value, ValidationContext validationContext)
+        {
+            if (value is IFormFile file)
+            {
+                var extension = Path.GetExtension(file.FileName);
+                if(!_supportedFormats.Contains(extension))
+                {
+                    return new ValidationResult("Unknown format");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
